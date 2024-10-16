fbd95cb363f3da753967a603c5f87977b1646d38
DCASSDGKU <dbc_bun@yahoo.com>
2024-10-16 02:39:02 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from "./components/navbar.jsx";
+import Footer from "./components/footer.jsx";
+import Catalog from "./pages/catalog.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>{/* Navbar */}
+      <Catalog></Catalog>{/* Catalog */}
+      <Footer></Footer>{/* Footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ footer.jsx
@@ -0,0 +1,16 @@
+import "./styles/footer.css";
+
+function footer(){
+    return(
+        <div className="footer">
+            <hr/>
+            <footer className="content">
+                <div>
+                    <span>Created by: Dennis Casiguran</span>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default footer;
